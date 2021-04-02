83e2509254bfca621bb3af656ff59d92941dda9c
Rodion Sibov <rodionsibov@gmail.com>
2021-04-02 09:14:04 +0300

    initial commit

---
+++ src/components/LoginModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <section
+    @click="$emit('close-login')"
+    class="h-screen w-screen z-20 bg-gray-500 fixed top-0 opacity-50"
+  ></section>
+  <div class="absolute inset-0">
+    <div class="flex h-full">
+      <div class="m-auto z-30 bg-white rounded shadow p-2">
+        <h1>Login</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['close-login']
+};
+</script>
+
+<style>
+</style>
