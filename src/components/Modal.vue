fe55efcc64bdccda611a02f54f4f1acf7e476ff4
Rodion Sibov <rodionsibov@gmail.com>
2021-04-21 18:49:56 +0300

    first commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <section
+    @click="close"
+    class="h-screen w-screen z-20 bg-gray-500 fixed top-0 left-0 opacity-50"
+  ></section>
+  <div class="absolute inset-0">
+    <div class="flex h-full">
+      <div class="m-auto z-30 bg-white rounded shadow p-2 w-1/3">
+        <div class="p-2 border"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    setup(){
+        
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/ReuseableModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="flex flex-wrap w-full">
+    <div class="m-auto">
+      <h1 class="text-2xl my-5">Reuseable Modal</h1>
+      <modal />
+    </div>
+  </section>
+</template>
+
+<script>
+import Modal from "../components/Modal.vue";
+
+export default {
+  components: { Modal },
+};
+</script>
+
+<style>
+</style>
