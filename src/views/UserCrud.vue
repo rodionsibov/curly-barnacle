a5aa6c2bf299d049b811c4703bcf51873404fc46
Rodion Sibov <rodionsibov@gmail.com>
2021-04-27 18:45:31 +0300

    update

--- src/views/UserCrud.vue
+++ src/views/UserCrud.vue
@@ -13,24 +13,22 @@
             <tr>
               <th class="px-4 py-2 border">ID</th>
               <th class="px-4 py-2 border">Avatar</th>
-              <th class="px-4 py-2 border">First Name</th>
-              <th class="px-4 py-2 border">Last Name</th>
+              <th class="px-4 py-2 border">Name</th>
               <th class="px-4 py-2 border">Email</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="user in state.users.data" :key="user.id">
-              <td class="border px-4 py-2">{{ user.id }}</td>
+            <tr v-for="user in state.users" :key="user._id">
+              <td class="border px-4 py-2">{{ user._id }}</td>
               <td class="border px-4 py-2">
                 <img
                   :src="user.avatar"
-                  :alt="user.first_name"
+                  :alt="user.name"
                   width="50"
                   class="rounded-full"
                 />
               </td>
-              <td class="border px-4 py-2">{{ user.first_name }}</td>
-              <td class="border px-4 py-2">{{ user.last_name }}</td>
+              <td class="border px-4 py-2">{{ user.name }}</td>
               <td class="border px-4 py-2">{{ user.email }}</td>
             </tr>
           </tbody>
@@ -63,6 +61,44 @@
   <teleport to="body">
     <modal v-if="isModalOpen" @close="isModalOpen = false">
       <template #title> Add New User </template>
+      <template #body>
+        <form @submit.prevent="submit">
+          <div class="p-2">
+            <label for="">Name</label>
+            <input
+              class="w-full p-2 rounded border"
+              type="text"
+              placeholder="User Name"
+              v-model="state.form.name"
+            />
+          </div>
+          <div class="p-2">
+            <label for="">Email</label>
+            <input
+              class="w-full p-2 rounded border"
+              type="email"
+              placeholder="User Email"
+              v-model="state.form.email"
+            />
+          </div>
+          <div class="p-2">
+            <label for="">Avatar</label>
+            <input
+              class="w-full p-2 rounded border"
+              type="text"
+              placeholder="Avatar Url"
+              v-model="state.form.avatar"
+            />
+          </div>
+          <div class="p-2">
+            <input
+              class="w-full p-2 rounded border hover:bg-gray-300"
+              type="submit"
+              value="Create"
+            />
+          </div>
+        </form>
+      </template>
     </modal>
   </teleport>
 </template>
@@ -78,13 +114,17 @@ export default {
     const isModalOpen = ref(false);
     const state = reactive({
       users: [],
+      form: {
+        name: "",
+        email: "",
+        avatar: "",
+      },
     });
 
     onMounted(async () => {
       const res = await fetch(`${process.env.VUE_APP_API_URL}/users`);
       const data = await res.json();
       state.users = data;
-      console.log(data);
     });
 
     async function next() {
@@ -99,7 +139,25 @@ export default {
       state.users = data;
     }
 
-    return { state, next, prev, isModalOpen };
+    async function submit() {
+      const res = await fetch(`${process.env.VUE_APP_API_URL}/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(state.form),
+      });
+      const data = await res.json()
+      console.log(data);
+      state.users.push(data);
+      state.form.name = "";
+      state.form.email = "";
+      state.form.avatar = "";
+      isModalOpen.value = false
+
+    }
+
+    return { state, next, prev, isModalOpen, submit };
   },
 };
 </script>
