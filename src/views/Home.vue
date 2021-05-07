b5fde8a2ad30d8335cf24bb62300eda0211b6c22
Rodion Sibov <60054035+rodionsibov@users.noreply.github.com>
2021-05-07 14:46:28 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-auto">
-    <h1 class="text-4xl font-bold mt-10 text-center">Welcome to Vue 3 Full Course</h1>
+    <h1 class="text-4xl font-bold mt-10 p-4 text-center">Welcome to Vue 3 Full Course</h1>
   </div>
 </template>
 
